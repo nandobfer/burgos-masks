f125a26c3430c389232a3638e5feb820d8dc4588
ispadilha <israel.padilha@gmail.com>
2024-02-05 10:15:57 -0300

    update: currency mask options

--- src/useCurrencyMask.ts
+++ src/useCurrencyMask.ts
@@ -1,14 +1,23 @@
 import createNumberMask from "text-mask-addons/dist/createNumberMask"
 
-export const useCurrencyMask = () => {
+interface params {
+    prefix?: string
+    suffix?: string
+    thousandsSeparatorSymbol?: string
+    decimalSymbol?: string
+    allowDecimal?: boolean
+    decimalLimit?: number
+}
+
+export const useCurrencyMask = (options?: params) => {
     // Create number mask with variable thousands separator and decimal point
     const currencyMask = createNumberMask({
-        prefix: "R$ ",
-        suffix: "",
-        thousandsSeparatorSymbol: ".",
-        decimalSymbol: ",",
-        allowDecimal: true,
-        decimalLimit: 2,
+        prefix: options?.prefix || "R$ ",
+        suffix: options?.suffix || "",
+        thousandsSeparatorSymbol: options?.thousandsSeparatorSymbol || ".",
+        decimalSymbol: options?.decimalSymbol || ",",
+        allowDecimal: options?.allowDecimal || true,
+        decimalLimit: options?.decimalLimit || 2,
     })
 
     return currencyMask
