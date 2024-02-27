71c1767fa7d8ad55263c16667c1e451075eccb58
nandobfer <nandobfer@yahoo.com.br>
2024-02-27 14:45:14 -0300

    update: more options

--- src/useNumberMask.ts
+++ src/useNumberMask.ts
@@ -8,14 +8,16 @@ export interface Options {
     suffix?: string
     allowNegative?: boolean
     decimalLimit?: number
+    thousandsSeparatorSymbol?: string
+    decimalSymbol?: string
 }
 
-export const useNumberMask = (options:Options) => {
+export const useNumberMask = (options: Options) => {
     const numberMask = createNumberMask({
         prefix: options.prefix || "",
         suffix: options.suffix || "",
-        thousandsSeparatorSymbol: ".",
-        decimalSymbol: ",",
+        thousandsSeparatorSymbol: options.thousandsSeparatorSymbol === undefined ? "." : options.thousandsSeparatorSymbol,
+        decimalSymbol: options.decimalSymbol === undefined ? "," : options.decimalSymbol,
         allowDecimal: options.allowDecimal || false,
         decimalLimit: options.decimalLimit || 2,
         integerLimit: options.limit || 0,
